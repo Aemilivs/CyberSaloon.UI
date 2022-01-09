49961b10633e5fa0e2f6e4998e956c3b70596699
Vadim Emelin <vemelin@sessionm.com>
2022-01-10 00:48:48 +0100

    Fix issues in UI.
    * Fix incorrect "Not found" message of Applications page
    * Fix visibility of button for creation of application in case if profile page is representing the client.
    * Introduce a margin for "Loading" spinner.
    * Change the reference to the profile page.

--- Client/Pages/Profile.razor.cs
+++ Client/Pages/Profile.razor.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using CyberSaloon.Client.Pages.Applicants.DTO;
 using CyberSaloon.Client.Pages.Artists.DTO;
 using CyberSaloon.Client.Pages.Common;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace CyberSaloon.Client.Pages
 {
@@ -14,15 +17,35 @@ namespace CyberSaloon.Client.Pages
         [Inject]
         public ICoreAPIClient Client { get; init; }
         
+        [Inject]
+        public AuthenticationStateProvider Provider { get; init; }
+
+        [Inject]
+        public NavigationManager Manager { get; init; }
+        
         public ArtistGetDTO ArtistDTO { get; set; } = default;
         public ApplicantGetDTO ApplicantDTO { get; set; } = default;
         public ProfileSelectedApplication Selection { get; set; } = ProfileSelectedApplication.None;
+        public bool IsOwned { get; set; } = false;
         public bool IsLoading { get; set; } = false;
         public bool IsFailed { get; set; } = false;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            IsOwned = await ProfileIsOwnedByClientAsync().ConfigureAwait(false);
+        }
 
         protected override async Task OnParametersSetAsync()
         {
             await base.OnParametersSetAsync();
+
+            if (string.IsNullOrEmpty(Alias))
+                Alias = await GetClientUsernameAsync().ConfigureAwait(false);
+
+            if (string.IsNullOrEmpty(Alias))
+                Manager.NavigateTo("/", false);
 
             IsFailed = false;
             IsLoading = false;
@@ -50,6 +73,43 @@ namespace CyberSaloon.Client.Pages
             }
 
             IsLoading = false;
+        }
+
+        private async Task<bool> ProfileIsOwnedByClientAsync()
+        {
+            var username = await GetClientUsernameAsync().ConfigureAwait(false);
+
+            if (string.IsNullOrEmpty(username))
+                return false;
+
+            return
+                string.Equals(
+                        Alias, 
+                        username,
+                        StringComparison.InvariantCulture
+                    ); 
+        }
+
+        private async Task<string> GetClientUsernameAsync()
+        {
+            var state = await Provider.GetAuthenticationStateAsync();
+
+            if (state == null)
+                return string.Empty;
+
+            var user = state.User;
+
+            if (user == default)
+                return string.Empty;
+
+            if (!user.Claims.Any())
+                return string.Empty;
+
+            return
+                user
+                     .Claims
+                     .Single(it => it.Type == "username")
+                     .Value;
         }
     }
 
